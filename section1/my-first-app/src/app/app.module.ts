ccf0ef4696171c510dc19b9e1d1cf41bf4d61ef2
codecamjam <camerontaghlabi@gmail.com>
2021-10-01 15:31:59 -0400

    l12 a basic project setup using bootstrap styling

--- section1/my-first-app/src/app/app.module.ts
+++ section1/my-first-app/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-//need to import this submodule so ngmodel works on the input element in app.component.html
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
