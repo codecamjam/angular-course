a490ff86bc4176f8bc3457ecbfff61a872fcf853
codecamjam <camerontaghlabi@gmail.com>
2021-10-01 17:13:59 -0400

    l23 fully understanding the component selector

--- section2-the-basics/my-first-app/src/app/servers/servers.component.ts
+++ section2-the-basics/my-first-app/src/app/servers/servers.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  //  selector: '[app-servers]', //this syntax is to select by attribute
+  // selector: '.app-servers', //select by class
   template: `<app-server></app-server> <app-server></app-server>`,
   styleUrls: ['./servers.component.css'],
 })
