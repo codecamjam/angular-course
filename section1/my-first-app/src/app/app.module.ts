a55eecdd4dc9b939a7b32694031cca455ae67be3
codecamjam <camerontaghlabi@gmail.com>
2021-10-01 15:10:15 -0400

    l7 editing the first app

--- section1/my-first-app/src/app/app.module.ts
+++ section1/my-first-app/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//need to import this submodule so ngmodel works on the input element in app.component.html
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
