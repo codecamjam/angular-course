1a77ef6c888208ef4c7471c77ce16d60446fe527
codecamjam <camerontaghlabi@gmail.com>
2021-10-01 22:06:38 -0400

    l24 assignment 1 practice components

--- section2-the-basics/my-first-app/src/app/servers/servers.component.ts
+++ section2-the-basics/my-first-app/src/app/servers/servers.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',
   //  selector: '[app-servers]', //this syntax is to select by attribute
   // selector: '.app-servers', //select by class
-  template: `<app-server></app-server> <app-server></app-server>`,
+  templateUrl: 'servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
