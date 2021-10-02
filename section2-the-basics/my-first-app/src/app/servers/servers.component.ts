ce6388edd4a7d9e8c68c23a8cf037d5f4f3d4c46
codecamjam <camerontaghlabi@gmail.com>
2021-10-01 22:53:31 -0400

    l30 bindable properties and events

--- section2-the-basics/my-first-app/src/app/servers/servers.component.ts
+++ section2-the-basics/my-first-app/src/app/servers/servers.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -14,4 +16,8 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
+  }
 }
