d8bfa4bb8c7f2f5c20b36098d9cfe9759fe5fab4
codecamjam <camerontaghlabi@gmail.com>
2021-10-02 15:46:32 -0400

    l43 getting the index when using ngFor

--- section2-the-basics/my-first-app/src/app/practice/practice.component.ts
+++ section2-the-basics/my-first-app/src/app/practice/practice.component.ts
@@ -7,10 +7,10 @@ import { Component } from '@angular/core';
 })
 export class PracticeComponent {
   display = false;
-  log: number[] = [];
+  log: Date[] = [];
 
   handleClick() {
     this.display = !this.display;
-    this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
 }
