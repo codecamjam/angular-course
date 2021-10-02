4fa1328068ca3d4f9ef0f984b21952ee8666e269
codecamjam <camerontaghlabi@gmail.com>
2021-10-01 22:38:30 -0400

    l27 property binding

--- section2-the-basics/my-first-app/src/app/servers/servers.component.ts
+++ section2-the-basics/my-first-app/src/app/servers/servers.component.ts
@@ -2,13 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  //  selector: '[app-servers]', //this syntax is to select by attribute
-  // selector: '.app-servers', //select by class
   templateUrl: 'servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-  constructor() {}
+  allowNewServer = false;
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {}
 }
