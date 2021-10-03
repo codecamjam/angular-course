0d043e18ef1d286b5d9cb586396c346b589251b1
codecamjam <camerontaghlabi@gmail.com>
2021-10-03 15:07:24 -0400

    l72 more on view encapsulation

--- section5-components-databinding-deepdive/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ section5-components-databinding-deepdive/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, ViewEncapsulation } from "@angular/core";
 
 @Component({
   selector: "app-server-element",
   templateUrl: "./server-element.component.html",
   styleUrls: ["./server-element.component.css"],
+  //Emulate is default, None means no encapsulation, ShadowDom/(formerly Native)
+  encapsulation: ViewEncapsulation.None,
 })
 export class ServerElementComponent implements OnInit {
   @Input("srvElement") element: { type: string; name: string; content: string };
