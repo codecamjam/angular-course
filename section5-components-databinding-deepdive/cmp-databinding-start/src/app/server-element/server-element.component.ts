b8e335f97ae04fcbe42905cc38ba10866892bb69
codecamjam <camerontaghlabi@gmail.com>
2021-10-04 12:20:07 -0400

    l78 seeing lifecycle hooks in action

--- section5-components-databinding-deepdive/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ section5-components-databinding-deepdive/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit, Input, ViewEncapsulation } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewEncapsulation,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+} from "@angular/core";
+import { ConsoleReporter } from "jasmine";
 
 @Component({
   selector: "app-server-element",
@@ -7,9 +21,64 @@ import { Component, OnInit, Input, ViewEncapsulation } from "@angular/core";
   //Emulate is default, None means no encapsulation, ShadowDom/(formerly Native)
   encapsulation: ViewEncapsulation.None,
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
   @Input("srvElement") element: { type: string; name: string; content: string };
-  constructor() {}
+  @Input() name: string;
 
-  ngOnInit(): void {}
+  constructor() {
+    console.log("constructor called");
+  }
+
+  // {element: SimpleChange}element: SimpleChangecurrentValue: content: "Just a test"name: "Testserver"type: "server"[[Prototype]]: ObjectfirstChange: truepreviousValue: undefined
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("ngOnChanges called");
+    console.log(changes);
+  }
+
+  ngOnInit(): void {
+    console.log("ngOnInit called");
+  }
+
+  //in development mode, on inital load, ngDoCheck is called
+  //a second time because of angular development mode
+  //also called if you click change first element button
+  ngDoCheck() {
+    console.log("ngDoCheck called");
+  }
+
+  //called after doCheck only once bc it doesnt get initialized again
+  //content is the thing we projected into this through ng-content
+  ngAfterContentInit() {
+    console.log("ngAfterContentInit called");
+  }
+
+  //called after do check bc its called after each change detection cycle
+  ngAfterContentChecked() {
+    console.log("ngAfterContentChecked called");
+  }
+
+  //called after content checked
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit called");
+  }
+
+  //called after content checked
+  ngAfterViewChecked() {
+    console.log("ngAfterViewChecked called");
+  }
+
+  ngOnDestroy() {
+    console.log("ngOnDestroy called");
+  }
 }
