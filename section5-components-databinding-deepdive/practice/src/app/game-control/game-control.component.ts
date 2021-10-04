d503d94205048962ee450382c70fa9ab3a19dcc6
codecamjam <camerontaghlabi@gmail.com>
2021-10-04 14:45:19 -0400

    l82 l83 practice

---
+++ section5-components-databinding-deepdive/practice/src/app/game-control/game-control.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css'],
+})
+export class GameControlComponent implements OnInit {
+  @Output() intervalFired = new EventEmitter<number>();
+  lastNumber = 0;
+  interval: any;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onStartGame() {
+    this.interval = setInterval(
+      () => this.intervalFired.emit(this.lastNumber++),
+      1000
+    );
+  }
+
+  onPauseGame() {
+    clearInterval(this.interval);
+  }
+}
