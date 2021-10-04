329010fb878f599d4cb43726245e990b8ade66eb
codecamjam <camerontaghlabi@gmail.com>
2021-10-04 17:02:34 -0400

    l96 using hostlistener to list to host events

--- section7-directives-deep-dive/src/app/better-highlight/better-highlight.directive.ts
+++ section7-directives-deep-dive/src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,10 @@
-import { Directive, OnInit, Renderer2, ElementRef } from '@angular/core';
+import {
+  Directive,
+  OnInit,
+  Renderer2,
+  ElementRef,
+  HostListener,
+} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]',
@@ -7,10 +13,26 @@ export class BetterHighlightDirective implements OnInit {
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   'background-color',
+    //   'blue'
+    // ); //optional flag options is 4th argument);
+  }
+
+  @HostListener('mouseenter') mouseOver(eventData: Event) {
     this.renderer.setStyle(
       this.elRef.nativeElement,
       'background-color',
       'blue'
+    ); //optional flag options is 4th argument);
+  }
+
+  @HostListener('mouseleave') mouseLeave(eventData: Event) {
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      'background-color',
+      'transparent'
     ); //optional flag options is 4th argument);
   }
 }
