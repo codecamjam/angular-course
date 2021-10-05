e34e52e1f03c4450fdb0e89c051e8a67bef4ac5d
codecamjam <camerontaghlabi@gmail.com>
2021-10-05 17:34:53 -0400

    l111 injecting services into services

--- section9-services-dependency-injection/src/app/accounts.service.ts
+++ section9-services-dependency-injection/src/app/accounts.service.ts
@@ -1,3 +1,12 @@
+import { Injectable } from "@angular/core";
+import { LoggingService } from "./logging.service";
+
+/* 
+note: you dont add @injectable to 
+the service you want to inject but instead to the service where
+you want to inject, so the receiving service
+*/
+@Injectable()
 export class AccountsService {
   accounts = [
     {
@@ -14,11 +23,15 @@ export class AccountsService {
     },
   ];
 
+  constructor(private loggingService: LoggingService) {}
+
   addAccount(name: string, status: string) {
     this.accounts.push({ name, status });
+    this.loggingService.logStatusChange(status);
   }
 
   updateStatus(id: number, status: string) {
     this.accounts[id].status = status;
+    this.loggingService.logStatusChange(status);
   }
 }
--- section9-services-dependency-injection/src/app/logging.service.ts
+++ section9-services-dependency-injection/src/app/logging.service.ts
@@ -1,3 +1,9 @@
+import { Injectable } from "@angular/core";
+
+//newer versions of angular recommend adding this decorator
+//to services regardless of if its used in the traditional manner
+//such as when you're injecting a service into another service
+@Injectable()
 export class LoggingService {
   logStatusChange(status: string) {
     console.log("A server status changed, new status: " + status);
