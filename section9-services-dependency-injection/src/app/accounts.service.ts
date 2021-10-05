13ffef791bd44ca99f162462e38b3a6934452599
codecamjam <camerontaghlabi@gmail.com>
2021-10-05 17:43:17 -0400

    l112 using services for cross-component communication

--- section9-services-dependency-injection/src/app/accounts.service.ts
+++ section9-services-dependency-injection/src/app/accounts.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from "@angular/core";
+import { Injectable, EventEmitter } from "@angular/core";
+
 import { LoggingService } from "./logging.service";
 
 /* 
@@ -22,6 +23,8 @@ export class AccountsService {
       status: "unknown",
     },
   ];
+
+  statusUpdated = new EventEmitter<string>();
 
   constructor(private loggingService: LoggingService) {}
 
