bd3e82b989c9163ebea223e5fa4da619d4cd5edb
codecamjam <camerontaghlabi@gmail.com>
2021-10-06 16:16:57 -0400

    l140 using query params practice

--- section11-routing/routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ section11-routing/routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Params } from "@angular/router";
 
 import { ServersService } from "../servers.service";
 
@@ -12,6 +12,7 @@ export class EditServerComponent implements OnInit {
   server: { id: number; name: string; status: string };
   serverName = "";
   serverStatus = "";
+  allowEdit = false;
 
   constructor(
     private serversService: ServersService,
@@ -22,7 +23,9 @@ export class EditServerComponent implements OnInit {
     // console.log(this.route.snapshot.queryParams);
     // console.log(this.route.snapshot.fragment);
     //dont need to unsubscribe these bc angular handles it apparently (just like params)
-    this.route.queryParams.subscribe();
+    this.route.queryParams.subscribe((queryParams: Params) => {
+      this.allowEdit = queryParams["allowEdit"] === "1" ? true : false;
+    });
     this.route.fragment.subscribe();
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
