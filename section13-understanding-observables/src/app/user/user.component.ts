852dbb018c9d9e17f4834aa070b415f8b35619ae
codecamjam <camerontaghlabi@gmail.com>
2021-10-09 16:40:11 -0400

    l171 getting closer to the core of observables

--- section13-understanding-observables/src/app/user/user.component.ts
+++ section13-understanding-observables/src/app/user/user.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params } from "@angular/router";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"],
 })
 export class UserComponent implements OnInit {
   id: number;
 
-  constructor(private route: ActivatedRoute) {
-  }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    //angular observables are taken care of (unsubscribed when no longer needed)
+    //automatically
     this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
     });
