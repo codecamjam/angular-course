bdc00ca9a001b7b245ad29e4ac5a30cc26f9358f
codecamjam <camerontaghlabi@gmail.com>
2021-10-09 17:00:34 -0400

    l173 errors and completion

--- section13-understanding-observables/src/app/home/home.component.ts
+++ section13-understanding-observables/src/app/home/home.component.ts
@@ -23,13 +23,37 @@ export class HomeComponent implements OnInit, OnDestroy {
         // observer.error() emit throw new error
         // observer.complete() when you are done
         observer.next(count);
+        // if (count == 2) {
+        if (count == 5) {
+          //observable will come to a hault
+          observer.complete();
+        }
+        if (count > 3) {
+          //DONT NEED TO UNSUBSCRIBE WHEN calling error
+          observer.error(new Error("Count is greater than 3!"));
+        }
         count++;
       }, 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe((data) => {
-      console.log(data);
-    });
+    this.firstObsSubscription = customIntervalObservable.subscribe(
+      (data) => {
+        //data handler function
+        console.log(data);
+      },
+      (error) => {
+        //error handler function
+        console.log(error);
+        alert(error.message);
+      },
+
+      () => {
+        //completion handler function
+        //here dont need to worry about unsubscribing although it wont hurt
+        console.log("completed");
+        //this does not run if error is thrown
+      }
+    );
   }
 
   ngOnDestroy(): void {
