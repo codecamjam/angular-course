9e337b8eee23b662d4fd1998de2be3961e3fc959
codecamjam <camerontaghlabi@gmail.com>
2021-11-06 22:05:41 -0400

    l212 reactive: creating custom validators

--- section15-forms/forms-reactive-start/src/app/app.component.ts
+++ section15-forms/forms-reactive-start/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from "@angular/core";
-import { FormArray, FormControl, FormGroup, Validators } from "@angular/forms";
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
 
 @Component({
   selector: "app-root",
@@ -9,11 +15,17 @@ import { FormArray, FormControl, FormGroup, Validators } from "@angular/forms";
 export class AppComponent implements OnInit {
   genders = ["male", "female"];
   signupForm: FormGroup;
+  forbiddenUsernames = ["Chris", "Anna"];
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl(null, Validators.required),
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNames.bind(this),
+        ]),
         email: new FormControl(null, [Validators.required, Validators.email]),
       }),
       gender: new FormControl("male"),
@@ -28,5 +40,14 @@ export class AppComponent implements OnInit {
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get("hobbies")).push(control);
+  }
+
+  //thats TS syntax for return {key: value} where value is t or f
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return { nameIsForbidden: true };
+    } else {
+      return null; //have to pass nothing or null, not {nameIsForbidden: false}
+    }
   }
 }
