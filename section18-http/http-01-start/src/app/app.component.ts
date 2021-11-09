837f3e8c97458c48d84ec0e3e6a5597ca9f988f5
codecamjam <camerontaghlabi@gmail.com>
2021-11-08 20:13:58 -0500

    l261 showing a loading indicator

--- section18-http/http-01-start/src/app/app.component.ts
+++ section18-http/http-01-start/src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Post } from "./post.model";
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -40,6 +41,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http
       .get<{ [key: string]: Post }>(
         "https://ng-complete-guide-5f9d7-default-rtdb.firebaseio.com/posts.json"
@@ -56,6 +58,7 @@ export class AppComponent implements OnInit {
         })
       )
       .subscribe((posts) => {
+        this.isFetching = false;
         this.loadedPosts = posts;
       });
   }
