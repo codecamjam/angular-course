2d0951f5d515734e4e2e3cf2636e4c940907a1d2
codecamjam <camerontaghlabi@gmail.com>
2021-11-08 19:39:03 -0500

    l256 sending a post request

---
+++ section18-http/http-01-start/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    // console.log(postData);
+    this.http
+      .post(
+        "https://ng-complete-guide-5f9d7-default-rtdb.firebaseio.com/posts.json",
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      }); //requests are only sent when you subscribe
+  }
+
+  onFetchPosts() {
+    // Send Http request
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+}
