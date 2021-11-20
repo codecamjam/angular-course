674faa31960e55960f07bdb64355fc0f7c387724
codecamjam <camerontaghlabi@gmail.com>
2021-11-19 21:26:39 -0500

    l334 preloading lazy-loaded code

--- section22-angular-modules/src/app/app-routing.module.ts
+++ section22-angular-modules/src/app/app-routing.module.ts
@@ -1,30 +1,30 @@
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-
-import { AuthComponent } from "./auth/auth.component";
+import { Routes, RouterModule, PreloadAllModules } from "@angular/router";
 
 const appRoutes: Routes = [
   { path: "", redirectTo: "/recipes", pathMatch: "full" },
   {
     path: "recipes",
     loadChildren: () =>
-      import("./recipes/recipes.module").then(m => m.RecipesModule)
+      import("./recipes/recipes.module").then((m) => m.RecipesModule),
   },
   {
     path: "shopping-list",
     loadChildren: () =>
       import("./shopping-list/shopping-list.module").then(
-        m => m.ShoppingListModule
-      )
+        (m) => m.ShoppingListModule
+      ),
   },
   {
     path: "auth",
-    loadChildren: () => import("./auth/auth.module").then(m => m.AuthModule)
-  }
+    loadChildren: () => import("./auth/auth.module").then((m) => m.AuthModule),
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
